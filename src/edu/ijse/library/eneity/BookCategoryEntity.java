44d5d2fa8366124f97136c7a785105c9eb8d2894
MalinduDulaj <malindudulaj16@gmail.com>
2024-07-17 10:08:06 +0530

    (BookCategory and Book) Entity class modified

--- src/edu/ijse/library/eneity/BookCategoryEntity.java
+++ src/edu/ijse/library/eneity/BookCategoryEntity.java
@@ -9,5 +9,25 @@ package edu.ijse.library.eneity;
  * @author malindudulaj
  */
 public class BookCategoryEntity {
+    private int categoryId;
+    private String categoryName;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    
+    
     
 }
--- src/edu/ijse/library/eneity/BookEntity.java
+++ src/edu/ijse/library/eneity/BookEntity.java
@@ -9,5 +9,69 @@ package edu.ijse.library.eneity;
  * @author malindudulaj
  */
 public class BookEntity {
-    
+    private int bookId;
+    private String title;
+    private String author;
+    private int publication_year;
+    private BookCategoryEntity category;
+    private int availablecopies;
+
+    public BookEntity(int bookId, String title, String author, int publication_year, BookCategoryEntity category, int availablecopies) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.publication_year = publication_year;
+        this.category = category;
+        this.availablecopies = availablecopies;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublication_year() {
+        return publication_year;
+    }
+
+    public BookCategoryEntity getCategory() {
+        return category;
+    }
+
+    public int getAvailablecopies() {
+        return availablecopies;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublication_year(int publication_year) {
+        this.publication_year = publication_year;
+    }
+
+    public void setCategory(BookCategoryEntity category) {
+        this.category = category;
+    }
+
+    public void setAvailablecopies(int availablecopies) {
+        this.availablecopies = availablecopies;
+    }
+
+     
 }
